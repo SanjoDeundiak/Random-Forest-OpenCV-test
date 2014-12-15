0cfdbbc6fd7f23d1bb08b4986ed88e4097a94c6c
Sanjo <Deundiak@gmail.com>
2014-12-15 03:08:31 +0200

    Finished?

--- Test/Test/Source.cpp
+++ Test/Test/Source.cpp
@@ -10,6 +10,7 @@ const int numVar = 9;
 // false = numerical, true - string
 static bool isString[17] = { false, false, false, true, true, false, false, false, true, false,
                              true, true, true, false, true, true, true };
+// true - not used
 static bool ignore[17] = { true, false, false, true, false, false, false, false, true, false,
                            true, false, true, false, true, true, true };
 
@@ -34,7 +35,7 @@ float Convert(const std::string& val, int i)
     if (val == "\"S\"")
         return 2.;
 
-    throw 3;
+    throw -1;
 }
 
 // This function reads data and responses from the file <filename>
@@ -55,13 +56,16 @@ int read_num_class_data(const char* filename, int var_count,
     seq = cvCreateSeq(0, sizeof(*seq), (var_count + 1)*sizeof(float), storage);
 
     std::string line, value, temp;
-    std::stringstream ss;
 
     while (f)
     {
         int j = 0;
+        line.clear();
         std::getline(f, line);
-        ss << line;
+        if (line.empty())
+            break;
+
+        std::stringstream ss(std::move(line));
         for (int i = 0; std::getline(ss, value, ','); i++)
         {
             if (isString[i])
@@ -76,27 +80,7 @@ int read_num_class_data(const char* filename, int var_count,
             el_ptr[j++] = Convert(value, i);
         }
         cvSeqPush(seq, el_ptr);
-        f.ignore(1);
     }
-
-    /*for (;;)
-    {
-        char* ptr;
-        if (!fgets(buf, M, f) || !strchr(buf, ','))
-            break;
-        el_ptr[0] = buf[0];
-        ptr = buf + 2;
-        for (i = 1; i <= var_count; i++)
-        {
-            int n = 0;
-            sscanf(ptr, "%f%n", el_ptr + i, &n);
-            ptr += n + 1;
-        }
-        if (i <= var_count)
-            break;
-        cvSeqPush(seq, el_ptr);
-    }
-    fclose(f);*/
 
     *data = cvCreateMat(seq->total, var_count, CV_32F);
     *responses = cvCreateMat(seq->total, 1, CV_32F);
@@ -143,7 +127,8 @@ int build_rtrees_classifier(char* data_filename,
 
     printf("The database %s is loaded.\n", data_filename);
     nsamples_all = data->rows;
-    ntrain_samples = (int)(nsamples_all*0.8);
+    //ntrain_samples = (int)(nsamples_all*0.8);
+    ntrain_samples = nsamples_all;
 
     // Create or load Random Trees classifier
     if (filename_to_load)
@@ -168,23 +153,12 @@ int build_rtrees_classifier(char* data_filename,
         cvSet(var_type, cvScalarAll(CV_VAR_ORDERED));
         cvSetReal1D(var_type, data->cols, CV_VAR_CATEGORICAL);
 
-        // 2. create sample_idx
-        sample_idx = cvCreateMat(1, nsamples_all, CV_8UC1);
-        {
-            CvMat mat;
-            cvGetCols(sample_idx, &mat, 0, ntrain_samples);
-            cvSet(&mat, cvRealScalar(1));
-
-            cvGetCols(sample_idx, &mat, ntrain_samples, nsamples_all);
-            cvSetZero(&mat);
-        }
-
         // 3. train classifier
         forest.train(data, // input data
             CV_ROW_SAMPLE, // data is in rows
             responses, // respones
             0, // all features are important
-            sample_idx, // ??
+            0,//sample_idx, // ??
             var_type, // indicates that we're solving classification problem
             0, // missing data mask
             CvRTParams
@@ -192,11 +166,11 @@ int build_rtrees_classifier(char* data_filename,
             10, // min sample count
             0, // regression accuracy
             false, // use surrogates ??
-            15, // max categories
-            0,
+            2, // max categories, used for clustering > 2 
+            0, // prior class probabilities
             true, // calculate var importance
-            4, // size of feature subset
-            100, // max num of trees in the forest
+            3, // size of feature subset ~sqrt(8)
+            150, // max num of trees in the forest
             0.01f, // accuracy
             CV_TERMCRIT_ITER  // termination type CV_TERMCRIT_ITER, CV_TERMCRIT_EPS, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS
             ) // CvRTParams
@@ -204,8 +178,10 @@ int build_rtrees_classifier(char* data_filename,
         printf("\n");
     }
 
+    printf("Train error %f\n", forest.get_train_error());
+
     // compute prediction error on train and test data
-    for (i = 0; i < nsamples_all; i++)
+    /*for (i = 0; i < nsamples_all; i++)
     {
         double r;
         CvMat sample;
@@ -223,7 +199,7 @@ int build_rtrees_classifier(char* data_filename,
     test_hr /= (double)(nsamples_all - ntrain_samples);
     train_hr /= (double)ntrain_samples;
     printf("Recognition rate: train = %.1f%%, test = %.1f%%\n",
-        train_hr*100., test_hr*100.);
+        train_hr*100., test_hr*100.);*/
 
     printf("Number of trees: %d\n", forest.get_tree_count());
 
@@ -236,21 +212,6 @@ int build_rtrees_classifier(char* data_filename,
         for (i = 0; i < var_importance->cols; i++)
             printf("%-2d\t%-4.1f\n", i,
             100.f*var_importance->data.fl[i] / rt_imp_sum);
-    }
-
-    //Print some proximitites
-    printf("Proximities between some samples corresponding to the letter 'T':\n");
-    {
-        CvMat sample1, sample2;
-        const int pairs[][2] = { { 0, 103 }, { 0, 106 }, { 106, 103 }, { -1, -1 } };
-
-        for (i = 0; pairs[i][0] >= 0; i++)
-        {
-            cvGetRow(data, &sample1, pairs[i][0]);
-            cvGetRow(data, &sample2, pairs[i][1]);
-            printf("proximity(%d,%d) = %.1f%%\n", pairs[i][0], pairs[i][1],
-                forest.get_proximity(&sample1, &sample2)*100.);
-        }
     }
 
     // Save Random Trees classifier to file if needed
@@ -269,12 +230,11 @@ int main(int argc, char *argv[])
 {
     char* filename_to_save = 0;
     char* filename_to_load = 0;
-    //char data_filename[] = "./newtrain.csv";
+    char data_filename[] = "./newtrain.csv";
     //char data_filename[] = "./letter-recognition.data";
-    char data_filename[] = "./test.csv";
+    //char data_filename[] = "./test.csv";
 
     build_rtrees_classifier(data_filename, filename_to_save, filename_to_load);
-    printf("Done\n");
 
     std::cin.ignore();
     return 0;
